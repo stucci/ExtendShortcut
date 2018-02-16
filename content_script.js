ae223c01082399f6e2e44e748b4d5ff1fc7cc5c5
stucci <sts.ucd@gmail.com>
2018-02-16 14:46:55 +0900

    add openEngWiki to swicth Wiki page Japanese and English

--- content_script.js
+++ content_script.js
@@ -2,7 +2,9 @@ var shortcut = {
     // Images -> All
     // A: function() { window.location.href = window.location.href.replace( /&tbm=isch/g, "") },
     // Check activeElement
-    E: function() { console.log(document.activeElement) },
+    A: function() { console.log(document.activeElement) },
+    // go English page of wikipedia
+    E: function() { openEngWiki() },
     // All -> iMages
     m: function() { window.location.href = window.location.href + "&tbm=isch" },
     // All -> Map
@@ -30,5 +32,31 @@ function openInBackground() {
     // Initialize mouse event to "Ctrl + Click"
     evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, true, false, false, false, 0, null);
     a.dispatchEvent(evt);
+}
+
+function openEngWiki() {
+    // var elements = document.getElementsByTagName("a");
+    var elements = document.getElementsByClassName("interlanguage-link interwiki-en");
+    if (elements.length) {
+        // var boolval = (elements != null)
+        // console.log(boolval);
+        // console.log(elements);
+        // console.log(elements.length);
+        // console.log(elements[0].firstChild.href);
+        window.location.href = elements[0].firstChild.href;
+    }
+    var elements = document.getElementsByClassName("interlanguage-link interwiki-ja");
+    console.log(elements);
+    if (elements.length) {
+        window.location.href = elements[0].firstChild.href;
+    }
+    // var elements = document.getElementsByClassName("interlanguage-link interwiki-en");
+    // console.log(elements);
+    // console.log(elements[0].firstChild);
+    // console.log(elements[0].firstChild.href);
+    // window.location.href = elements[0].firstChild.href
+    // console.log(elements[5].text);
+    // console.log(elements.indexOf("English"));
+    // assert(elements);
 }
 
